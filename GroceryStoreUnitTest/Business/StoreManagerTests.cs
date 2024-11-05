fe65e2c7d72d2c3115bdce2fe7741681135806a0
sebastienbeauvais <sbeauvais7@gmail.com>
2024-11-04 18:04:13 -0800

    fix store manager tests

--- GroceryStoreUnitTest/Business/StoreManagerTests.cs
+++ GroceryStoreUnitTest/Business/StoreManagerTests.cs
@@ -1,6 +1,8 @@
 using GroceryStore.Business.Interfaces;
 using GroceryStore.Business.Service;
+using GroceryStore.Data.Interfaces;
 using GroceryStore.Models;
+using GroceryStore.Models.Interfaces;
 using Moq;
 
 namespace GroceryStoreUnitTest.Business
@@ -8,70 +10,67 @@ namespace GroceryStoreUnitTest.Business
     [TestClass]
     public class StoreManagerTests
     {
-        private StoreManager _storeManager;
+        private StoreManager storeManager;
         private Mock<ICashRegister> _mockCashRegister;
         private Mock<IShoppingCartHandler> _mockShoppingCart;
-        private List<StoreItem> _storeInventory;
+        private Mock<IStoreInventoryDb> _mockStoreDb;
+        private List<IStoreItem> _storeInventory;
 
-        /*[TestInitialize]
+        [TestInitialize]
         public void Setup()
         {
             _mockCashRegister = new Mock<ICashRegister>();
-            _mockShoppingCart = new Mock<IShoppingCartHelper>();
-            _storeManager = new StoreManager(_mockCashRegister.Object, _mockShoppingCart.Object);
+            _mockShoppingCart = new Mock<IShoppingCartHandler>();
+            _mockStoreDb = new Mock<IStoreInventoryDb>();
 
-            _storeInventory = new List<StoreItem>
+            _storeInventory = new List<IStoreItem>
             {
                 new StoreItem { Id = 1, Name = "Apple", Price = 0.50 },
                 new StoreItem { Id = 2, Name = "Banana", Price = 0.20 }
             };
-        }
 
+            _mockStoreDb.Setup(x => x.Inventory).Returns(_storeInventory);
+
+            storeManager = new StoreManager(_mockCashRegister.Object, _mockShoppingCart.Object, _mockStoreDb.Object);
+        }
+        
         [TestMethod]
-        public void HandleUserInput_ShowInventory_DisplaysInventory()
+        public void HandleUserInput_CallsShowsStoreInventoryToConsole()
         {
             using (var sw = new StringWriter())
             {
                 Console.SetOut(sw);
 
-                _storeManager.HandleUserInput("1");
+                storeManager.HandleUserInput("1");
 
                 var output = sw.ToString();
                 Assert.IsTrue(output.Contains("Apple"));
                 Assert.IsTrue(output.Contains("Banana"));
             }
         }
-
+        
         [TestMethod]
-        public void HandleUserInput_AddItemToCart_CallsAddItemOnShoppingCart()
+        public void HandleUserInput_CallsAddItemOnShoppingCart()
         {
-            _storeManager.HandleUserInput("2");
+            storeManager.HandleUserInput("2");
 
-            _mockShoppingCart.Verify(cart => cart.AddItemToShoppingCart(It.IsAny<IEnumerable<StoreItem>>()), Times.Once);
+            _mockShoppingCart.Verify(cart => cart.AddItemToShoppingCart(), Times.Once);
         }
-
+        
         [TestMethod]
-        public void HandleUserInput_ShowCartItems_CallsShowItemsOnShoppingCart()
+        public void HandleUserInput_CallsShowItemsOnShoppingCart()
         {
-            _storeManager.HandleUserInput("3");
+            storeManager.HandleUserInput("3");
 
-            _mockShoppingCart.Verify(cart => cart.ShowItemsInShoppingCart(It.IsAny<IEnumerable<StoreItem>>()), Times.Once);
+            _mockShoppingCart.Verify(cart => cart.ShowItemsInShoppingCart(), Times.Once);
         }
-
+        
         [TestMethod]
-        public void HandleUserInput_Checkout_CallsCheckoutOnCashRegister()
+        public void HandleUserInput_CallsCheckoutOnCashRegister()
         {
-            var cartItems = new List<CartItem>
-            {
-                new CartItem { Name = "Apple", Quantity = 2, Price = 0.50 }
-            };
+            storeManager.HandleUserInput("4");
 
-            _mockShoppingCart.Setup(cart => cart.GetShoppingCartItems()).Returns(cartItems);
-            _mockShoppingCart.Setup(cart => cart.GetCartTotal(It.IsAny<IEnumerable<StoreItem>>(), cartItems)).Returns(1.0);
-
-            _storeManager.HandleUserInput("4");
-
-            _mockCashRegister.Verify(register => register.Checkout(cartItems, 1.0), Times.Once);
-        }*/
+            _mockCashRegister.Verify(register => register.Checkout(), Times.Once);
+        }
     }
 }
